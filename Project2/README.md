47d7a52e3cfd6a3a8f5b6eebb019d0dacf37c18d
Carina Adrianzen <70672577+carinadyann@users.noreply.github.com>
2023-09-15 08:12:51 -0700

    create new villager feature completed

--- Project2/README.md
+++ Project2/README.md
@@ -5,3 +5,7 @@ Animal Crossing New Horizons: "Adopt a Villager" website
 Figma:
 
 https://www.figma.com/file/GufZLWsvxOvwkS0og3QHlv/AdoptAVillager?type=design&node-id=0%3A1&mode=design&t=2IW7iR1tUKdpKXmS-1
+
+Resources:
+
+https://www.scaler.com/topics/javascript-sort-an-array-of-objects/
--- Project2/main.js
+++ Project2/main.js
@@ -8,7 +8,7 @@ class Villager {
         gifted = "Yes",
         boxes = "No"
     ) {
-        this.img_ref = villagerImg.get(name);
+        this.img_ref = "/Assets/ankha.png";
         this.name = name;
         this.type = type;
         this.personality = personality;
@@ -17,29 +17,41 @@ class Villager {
     }
 }
 
-var villagerImg = new Map([
-    ["Anhka", "/Assets/ankha.png"],
-    ["Apple", "/Assets/apple.png"],
-    ["Audie", "/Assets/audie.png"],
-    ["Cherry", "/Assets/cherry.png"],
-    ["Diana", "/Assets/diana.png"],
-    ["Gayle", "/Assets/gayle.png"],
-    ["Lucky", "/Assets/lucky.png"],
-    ["Maple", "/Assets/maple.png"],
-    ["Margie", "/Assets/margie.png"],
-    ["Marshal", "/Assets/marshal.png"],
-    ["Mint", "/Assets/mint.png"],
-    ["Pekoe", "/Assets/pekoe.png"],
-    ["Pinky", "/Assets/pinky.png"],
-    ["Poppy", "/Assets/poppy.png"],
-    ["Punchy", "/Assets/punchy.png"],
-    ["Roald", "/Assets/roald.png"],
-    ["Apollo", "/Assets/apollo.png"],
+class villagerRef {
+    constructor(
+        img_ref_indv = "",
+        type_indv = "",
+        personality_indv = ""
+    ) {
+        this.img_ref_indv = img_ref_indv;
+        this.type_indv = type_indv;
+        this.personality_indv = personality_indv;
+    }
+}
+
+var villagerDB = new Map([
+    ["Anhka", new villagerRef("/Assets/ankha.png", "Cat", "Snooty")],
+    ["Apple", new villagerRef("/Assets/apple.png", "Hamster", "Peppy")],
+    ["Audie", new villagerRef("/Assets/audie.png", "Wolf", "Peppy")],
+    ["Cherry", new villagerRef("/Assets/cherry.png", "Dog", "Sisterly")],
+    ["Diana", new villagerRef("/Assets/diana.png", "Deer", "Snooty")],
+    ["Gayle", new villagerRef("/Assets/gayle.png", "Alligator", "Normal")],
+    ["Lucky", new villagerRef("/Assets/lucky.png", "Dog", "Lazy")],
+    ["Maple", new villagerRef("/Assets/maple.png", "Cub", "Normal")],
+    ["Margie", new villagerRef("/Assets/margie.png", "Elephant", "Normal")],
+    ["Marshal", new villagerRef("/Assets/marshal.png", "Squirrel", "Smug")],
+    ["Mint", new villagerRef("/Assets/mint.png", "Squirrel", "Snooty")],
+    ["Pekoe", new villagerRef("/Assets/pekoe.png", "Cub", "Normal")],
+    ["Pinky", new villagerRef("/Assets/pinky.png", "Bear", "Peppy")],
+    ["Poppy", new villagerRef("/Assets/poppy.png", "Squirrel", "Normal")],
+    ["Punchy", new villagerRef("/Assets/punchy.png", "Cat", "Lazy")],
+    ["Roald", new villagerRef("/Assets/roald.png", "Penguin", "Jock")],
+    ["Apollo", new villagerRef("/Assets/apollo.png", "Eagle", "Cranky")],
 ])
 
 let arrayVillagers = [
     {
-        img_ref: villagerImg.get("Mint"),
+        img_ref: villagerDB.get("Mint").img_ref_indv,
         name: "Mint",
         type: "Squirrel",
         personality: "Snooty",
@@ -47,7 +59,7 @@ let arrayVillagers = [
         boxes: "Yes" 
     },
     {
-        img_ref: villagerImg.get("Roald"),
+        img_ref: villagerDB.get("Roald").img_ref_indv,
         name: "Roald",
         type: "Penguin",
         personality: "Jock",
@@ -55,7 +67,7 @@ let arrayVillagers = [
         boxes: "Yes" 
     },
     {
-        img_ref: villagerImg.get("Punchy"),
+        img_ref: villagerDB.get("Punchy").img_ref_indv,
         name: "Punchy",
         type: "Cat",
         personality: "Lazy",
@@ -66,7 +78,7 @@ let arrayVillagers = [
 
 let elGrid = document.getElementById("villagers-grid");
 
-function createVillagers(villager) {
+function createVillagerCard(villager) {
     const villagerCard = document.createElement("div");
     //villagerCard.classList.add('villager-card')
 
@@ -88,14 +100,6 @@ function createVillagers(villager) {
     el_gifted.textContent = `Gifted: ${villager.gifted}`;
     el_boxes.textContent = `In Boxes: ${villager.boxes}`;
 
-    villagerCard.appendChild(el_image);
-    villagerCard.appendChild(el_name);
-    villagerCard.appendChild(el_type);
-    villagerCard.appendChild(el_personality);
-    villagerCard.appendChild(el_gifted);
-    villagerCard.appendChild(el_boxes);
-    elGrid.appendChild(villagerCard);
-
     el_image.classList.add("cardContent");
     el_name.classList.add("cardContent");
     el_type.classList.add("cardContent");
@@ -103,28 +107,82 @@ function createVillagers(villager) {
     el_gifted.classList.add("cardContent");
     el_boxes.classList.add("cardContent");
 
+    villagerCard.appendChild(el_image);
+    villagerCard.appendChild(el_name);
+    villagerCard.appendChild(el_type);
+    villagerCard.appendChild(el_personality);
+    villagerCard.appendChild(el_gifted);
+    villagerCard.appendChild(el_boxes);
+
     villagerCard.classList.add("villagers");
+
+    elGrid.appendChild(villagerCard);
 }
 
 function updateGrid() {
+    elGrid.innerHTML = "";
     arrayVillagers.forEach((villager) => {
-        createVillagers(villager)
+        createVillagerCard(villager)
     });
 }
 
 function addNewVillager() {
-    const newVillager = new Object();
-    newVillager.name = document.getElementById(""); // not done
+    const newVillager = new Villager();
+    const formName = String(document.getElementById("vname").value);
 
+    newVillager.img_ref = villagerDB.get(formName).img_ref_indv;
+    newVillager.name = formName;
+    newVillager.type = villagerDB.get(formName).type_indv;
+    newVillager.personality = villagerDB.get(formName).personality_indv;
+
+    var giftGroup = document.getElementsByName("gifted");
+    var checkGifted = Array.from(giftGroup).find(
+        (radio) => radio.checked
+    );
+    newVillager.gifted = checkGifted.value;
+
+    var boxGroup = document.getElementsByName("boxes");
+    var checkBoxes = Array.from(boxGroup).find(
+        (radio) => radio.checked
+    );
+    newVillager.boxes = checkBoxes.value;
+
+    arrayVillagers.push(newVillager);
     updateGrid();
 }
 
-updateGrid();
+// select update
+function updateTraits(sel) {
+    var selected = String(sel.options[sel.selectedIndex].text);
+    document.getElementById("typeAuto").innerHTML = villagerDB.get(selected).type_indv;
+    document.getElementById("personalityAuto").innerHTML = villagerDB.get(selected).personality_indv;
+}
+
+function sortVillagers(sel) {
+    switch(sel) {
+        case ascending:
+            arrayVillagers.sort((p1, p2) => (p1.name > p2.name ? 1 : (p1.name > p2.name) ? -1 : 0));
+        case descending:
+
+        case newest:
+
+        case oldest:
+
+        case typeA:
+
+        case typeD:
+
+        default:
+
+    }
+    updateGrid();
+}
 
 // modal setup
 var modal = document.getElementById("myModal");
 var btn = document.getElementById("modalBtn");
 var span = document.getElementsByClassName("close")[0];
+var submit = document.getElementById("subBtn");
 
 btn.onclick = function() {
     modal.style.display = "block";
@@ -140,11 +198,14 @@ window.onclick = function(event) {
     }
 }
 
+submit.onclick = function() {
+    modal.style.display = "none";
+    // document.getElementById("myForm").reset();
+}
+
 // accordian animation setup
 var acc = document.getElementsByClassName("accordian");
-var i;
-
-for (i = 0; acc.length; i++) {
+for (let i = 0; i < acc.length; i++) {
     acc[i].addEventListener("click", function() {
         this.classList.toggle("active");
 
